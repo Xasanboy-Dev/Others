9f5f17b03551da7fa87d322ebf40a9f772f65ab7
Xasanboy <hasanbojabdurasulov309@gmail.com>
2022-11-12 21:09:54 +0500

    Begin

--- app.js
+++
@@ -1,8 +0,0 @@
-const vars = require("./script")
-vars.x = 45
-vars.printX()
-vars.changeX(0)
-vars.printX()
-console.log("x = ", vars.x)
-console.log("y = ", vars.y)
-console.log("Started: app.js")
